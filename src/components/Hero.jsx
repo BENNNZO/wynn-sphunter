cf05b81d73cf65510a080ac3f5aca65a6d5c6e88
Benjamin Phillips <benjamingt537@gmail.com>
2024-04-13 23:05:12 -0400

    got axios and maps working also started working on skeletons for loading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Hero() {
     return (
-        <section className=''>
-            
+        <section className='grid place-items-center h-32'>
+            <h1 className='text-5xl'>Wynncraft Soul Point Tracker</h1>
         </section>
     )
 }
