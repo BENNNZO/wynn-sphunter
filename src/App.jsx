4fa4b4a182146dc4e2d6f1f32f0b3c936daebdc2
Benjamin Phillips <benjamingt537@gmail.com>
2024-04-14 01:42:35 -0400

    added a note talking about desync for the user

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import World from './components/World';
 
 function App() {
 	const [data, setData] = useState(null)
+	const [note, setNote] = useState(true)
 
 	function msToTimeLeft(duration) {
         let seconds = Math.floor((duration / 1000) % 60)
@@ -18,27 +19,27 @@ function App() {
     }
 
 	useEffect(() => {
-		axios.get('https://athena.wynntils.com/cache/get/serverList')
-		.then(res => {
-			// probably is a better way to do this but this is the best I could do
-			let sortedData = [...Object.entries(res.data["servers"])].sort(function (a, b) {
-				let keyA = new Date(msToTimeLeft(a[1]['firstSeen']))
-				let keyB = new Date(msToTimeLeft(b[1]['firstSeen']))
+		// axios.get('https://athena.wynntils.com/cache/get/serverList')
+		// .then(res => {
+		// 	// probably is a better way to do this but this is the best I could do
+		// 	let sortedData = [...Object.entries(res.data["servers"])].sort(function (a, b) {
+		// 		let keyA = new Date(msToTimeLeft(a[1]['firstSeen']))
+		// 		let keyB = new Date(msToTimeLeft(b[1]['firstSeen']))
 				
-				if (keyA < keyB) return -1
-				if (keyA > keyB) return 1
+		// 		if (keyA < keyB) return -1
+		// 		if (keyA > keyB) return 1
 				
-				return 0
-			});
+		// 		return 0
+		// 	});
 
-			console.log(res.data["servers"])
-			console.log(sortedData)
-			setData(sortedData)
-		})
+		// 	console.log(res.data["servers"])
+		// 	console.log(sortedData)
+		// 	setData(sortedData)
+		// })
 	}, [])
 
 	return (
-		<div className='bg-zinc-950 min-h-screen text-white px-10'>
+		<div className='bg-zinc-950 min-h-screen text-white px-10 relative overflow-hidden'>
 			<Hero />
 			<span className='block w-2/5 h-px bg-gradient-to-r from-transparent via-white/10 to-transparent m-auto' />
 			<section className='flex flex-col gap-2 items-center col-span-2 max-w-3xl mx-auto py-4 md:py-10'>
@@ -59,6 +60,23 @@ function App() {
 				)}
 			</section>
 			<span className='block w-2/5 h-px bg-gradient-to-r from-transparent via-white/10 to-transparent m-auto' />
+			{
+				note ? (
+					<div className='note bg-zinc-800 px-3 py-2 rounded-md border border-zinc-600 shadow-lg w-9/12 max-w-2xl'>
+						<div onClick={(e) => {
+							e.preventDefault()
+
+							setNote(false)
+						}} className='bg-red-400 rounded-full w-5 h-5 absolute bottom-full left-full -translate-x-1/2 translate-y-1/2 cursor-pointer shadow-md border-2 border-red-300'>
+							<span className='absolute w-0.5 h-2/3 rounded-full rotate-45 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white'></span>
+							<span className='absolute w-0.5 h-2/3 rounded-full -rotate-45 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white'></span>
+						</div>
+						<p className='text-zinc-400'><strong>Note:</strong> Depending on how long a worlds uptime it can cause a desync so if one world is wrong just move on to the next world.</p>
+					</div>
+				) : (
+					null
+				)
+			}
 		</div>
 	);
 }
